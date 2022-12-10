dba7f74b6b0c080bdce8b016d4cbf72e2a1573f6
tt <**>
2022-12-10 19:54:08 +0800

    feat: 完成成绩查询，成绩通知和预警的后端操作
    由于未统一类名，为了不和其他人的干扰，我自行加了后缀，ByTang。这个并不规范。如果后期需要合并冗余可以删去该后缀。

---
+++ src/main/java/cn/edu/usst/spm/controller/GradeManageControllerByTang.java
@@ -0,0 +1,72 @@
+package cn.edu.usst.spm.controller;
+
+import cn.edu.usst.spm.bean.LoginUser;
+import cn.edu.usst.spm.bean.vo.GradeInfoVO;
+import cn.edu.usst.spm.service.GradeManageServiceByTang;
+import cn.edu.usst.spm.util.Constant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class GradeManageControllerByTang {
+
+    @Autowired
+    GradeManageServiceByTang gradeManageServiceByTang;
+
+    @GetMapping("/grade-manage/grades")
+    public ResponseEntity<List<GradeInfoVO>> showGrades(HttpSession session) {
+        LoginUser user = (LoginUser) session.getAttribute(Constant.USER);
+
+        // 不是老师，返回403拒绝访问
+        if (!user.isTeacher()) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        List<GradeInfoVO> grades = gradeManageServiceByTang.getGrades(1);
+        return new ResponseEntity<>(grades, HttpStatus.OK);
+    }
+
+    @GetMapping("/grade-manage/show-grade")
+    public ResponseEntity<GradeInfoVO> showGrade(HttpSession session) {
+        LoginUser user = (LoginUser) session.getAttribute(Constant.USER);
+
+        // 不是学生，返回403拒绝访问
+        if (user.isTeacher()) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        GradeInfoVO grade = gradeManageServiceByTang.getGrade(user.getId());
+        return new ResponseEntity<>(grade, HttpStatus.OK);
+    }
+
+    @PostMapping("/grade-manage/send-grade-info")
+    public ResponseEntity<Integer> sendText(@RequestBody List<Integer> receivers, HttpSession session) {
+        LoginUser user = (LoginUser) session.getAttribute(Constant.USER);
+        // 不是老师，403拒绝
+        if (!user.isTeacher()) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        if (receivers != null && receivers.size() > 0) {
+            gradeManageServiceByTang.sendGradeInfo(receivers);
+        }
+        return new ResponseEntity<>(0, HttpStatus.OK);
+    }
+
+    @PostMapping("/grade-manage/send-grade-warning")
+    public ResponseEntity<Integer> sendWarning(@RequestBody List<Integer> receivers, HttpSession session) {
+        LoginUser user = (LoginUser) session.getAttribute(Constant.USER);
+        // 不是老师，403拒绝
+        if (!user.isTeacher()) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        if (receivers != null && receivers.size() > 0) {
+            gradeManageServiceByTang.sendGradeWarning(receivers);
+        }
+        return new ResponseEntity<>(0, HttpStatus.OK);
+    }
+}
