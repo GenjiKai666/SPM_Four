e5c2700a6a1292736c9f82b45a4029883104665b
genji <jesse_aarons@foxmail.com>
2022-12-11 19:34:50 +0800

    fix: 修复了没有找到用户后会返回空的bug

--- src/main/java/cn/edu/usst/spm/controller/AssignmentController.java
+++ src/main/java/cn/edu/usst/spm/controller/AssignmentController.java
@@ -89,11 +89,13 @@ public class AssignmentController {
     }
     @RequestMapping(value = "/assignment/getuserinfo",method = RequestMethod.GET)
     public LoginCheck getUserInfo(HttpSession session){
+        session.setAttribute("username","李四");
+        session.setAttribute("isTeacher",true);
         String username = (String)session.getAttribute("username");
         Boolean isTeacher = (Boolean) session.getAttribute("isTeacher");
         Integer teacher;
         if(isTeacher == null || username == null){
-            return null;
+            return new LoginCheck(null,null);
         }
         Integer id = null;
         if(isTeacher){
