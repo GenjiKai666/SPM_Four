6c519abb5ec20cdb3137cef5bc4981fb44bd59bb
tt <**>
2022-12-13 18:12:09 +0800

    fix: 由于GROUP是关键字，修改GROUP为GROUP01
    以及添加STUDENT_TEACHER中对于STUDENT_ID的约束，防止学生重复。

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -30,7 +30,9 @@ create table if not exists
     TEACHER_ID   int     not null references TEACHER (`ID`),
     IS_CONFIRMED boolean not null default false,
     constraint STUDENT_TEACHER_UNIQUE
-        unique (STUDENT_ID, TEACHER_ID)
+        unique (STUDENT_ID, TEACHER_ID),
+    constraint STUDENT_TEACHER_UNIQUE1
+        unique (STUDENT_ID)
 ) charset = utf8mb4;
 
 create table if not exists
@@ -68,7 +70,7 @@ create table if not exists
 
 -- 选课之后，创建的小组，LEADER_ID 记录组长的id
 create table if not exists
-    `GROUP`
+    `GROUP01`
 (
     ID        int         not null auto_increment primary key,
     NAME      varchar(30) not null unique,
@@ -83,6 +85,6 @@ create table if not exists
     `GROUP_MEMBER`
 (
     ID                 int not null auto_increment primary key,
-    GROUP_ID           int not null references `GROUP` (`ID`),
+    GROUP_ID           int not null references `GROUP01` (`ID`),
     STUDENT_TEACHER_ID int not null unique references `STUDENT_TEACHER` (`ID`)
 ) charset = utf8mb4;
